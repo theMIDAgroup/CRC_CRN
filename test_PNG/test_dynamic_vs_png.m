ed4505e23f344ba92f4459c03a1c4c3de5236b04
Sara <sommariva@dima.unige.it>
2022-02-18 15:16:45 +0100

    Plotted results via boxplot

--- test_PNG/test_dynamic_vs_png.m
+++ test_PNG/test_dynamic_vs_png.m
@@ -26,8 +26,8 @@ v_phys = CRN.matrix.v;
 ind_one = size(CRN.matrix.S, 1) + 1;
 
 % 1.3. Mutations to be considered
-perc = 0;
-lof_mutations = {'TBRII', 'SMAD4', 'Cadh', 'APC', 'PTEN', 'AKT', 'ARF'};
+perc = 0; aux_n_runs = 50;
+lof_mutations = {'TBRII', 'SMAD4', 'PTEN', 'AKT'};%'Cadh', 'APC', , 'ARF'};
 gof_mutations = {'Raf', 'Ras', 'PI3K', 'BetaCatenin'};
 lof_mutations_type2 = {'TP53'};
 all_mutations = [gof_mutations, lof_mutations, lof_mutations_type2];
@@ -37,6 +37,11 @@ aux_dyn_file = 'dyn_mut_%s_test1.mat';
 aux_png_file = 'png_mut_%s_test1.mat';
 
 file_table = fullfile(folder_res, 'table_dyn_vs_png.txt');
+
+bp_elapsed_time_png = zeros(aux_n_runs, 1+n_mutations);
+bp_elapsed_time_dyn = zeros(aux_n_runs, 1+n_mutations);
+bp_fxeq_png = zeros(aux_n_runs, 1+n_mutations);
+bp_fxeq_dyn = zeros(aux_n_runs, 1+n_mutations);
 
 %% Step 1. physiological cell
 % 1.1. Load
@@ -100,6 +105,15 @@ fileID = fopen(file_table,'w');
 fprintf(fileID, 'Physiological &  %2.2e $\\pm$ %2.2e & %2.2e $\\pm$ %2.2e & %2.2f $\\pm$ %2.2f & %2.2f $\\pm$ %2.2f \\\\ \n', ...
     mean_f_png, std_f_png, mean_f_dyn, std_f_dyn, ...
     mean_elapsed_png, std_elapsed_png, mean_elapsed_dyn, std_elapsed_dyn);
+
+%       Boxplot
+bp_elapsed_time_dyn(:, 1) = elapsed_time_dyn;
+bp_elapsed_time_png(:, 1) = elapsed_time_png;
+bp_fxeq_dyn(:, 1) = f_dyn;
+bp_fxeq_png(:, 1) = f_png;
+cond_name = {'Phys'};
+
+clear elapsed_time_dyn elapsed_time_png f_dyn f_png
 
 %% Step 2. Mutated cell
 for im = 1:n_mutations
@@ -172,6 +186,13 @@ for im = 1:n_mutations
     fprintf(fileID, '%s & %2.2e $\\pm$ %2.2e & %2.2e $\\pm$ %2.2e & %2.2f $\\pm$ %2.2f & %2.2f $\\pm$ %2.2f \\\\ \n', ...
     protein, mean_f_png, std_f_png, mean_f_dyn, std_f_dyn, ...
     mean_elapsed_png, std_elapsed_png, mean_elapsed_dyn, std_elapsed_dyn);
+
+    %       Boxplot
+    bp_elapsed_time_dyn(:, 1+im) = elapsed_time_dyn;
+    bp_elapsed_time_png(:, 1+im) = elapsed_time_png;
+    bp_fxeq_dyn(:, 1+im) = f_dyn;
+    bp_fxeq_png(:, 1+im) = f_png;
+    cond_name{im+1} = protein;
     
     clear elapsed_time_dyn elapsed_time_png dyn_mut png_mut f_dyn f_png ...
         mean_elapsed_dyn mean_elapsed_png std_elapsed_dyn std_elapsed_dyn ...
@@ -180,40 +201,43 @@ for im = 1:n_mutations
     pause
     
 end
- fclose(fileID);
+fclose(fileID);
 
-
-% 
-% %       Average and std over runs
-% mean_f_dyn = mean(f_dyn);
-% std_f_dyn = std(f_dyn);
-% mean_f_png = mean(f_png);
-% std_f_png = std(f_png);
-% 
-% % 1.4. Plot
-% figure
-% subplot(2, 1, 1)
-% plot(1:numel(elapsed_time_dyn), elapsed_time_dyn, 'b*', 'linewidth', 2, ...
-%     'Displayname', 'Dynamic')
+%% Make boxplot
+% f_bp = figure('units','normalized','outerposition',[0 0 1 0.5]);
+% aux_position = 1:(n_mutations+1);
+% h = boxplot(bp_elapsed_time_png, 'Color', 'b',  ...
+%         'position', aux_position-0.1, 'Symbol', 'ob', 'width', 0.3);
+% set(h,{'linew'},{2})
 % hold on
-% plot(1:numel(elapsed_time_png), elapsed_time_png, 'r*', 'linewidth', 2, ...
-%     'Displayname', 'PNG')
-% legend('show')
-% xlabel('Runs')
-% ylabel('Elsapsed time [sec]')
-% title('Physiological')
-% subplot(2, 1, 2)
-% plot(1:numel(elapsed_time_dyn), f_dyn, 'b*', 'linewidth', 2, ...
-%     'Displayname', 'Dynamic')
-% hold on
-% plot(1:numel(elapsed_time_png), f_png, 'r*', 'linewidth', 2, ...
-%     'Displayname', 'PNG')
-% legend('show')
-% xlabel('Runs')
-% ylabel('f(x_{eq})')
-% 
-% %       Table
-% fprintf('Physiological & %2.2f $\\pm$ %2.2f & %2.2f $\\pm$ %2.2f &  %2.2e $\\pm$ %2.2e & %2.2e $\\pm$ %2.2e &  \n ', ...
-%     mean_elapsed_png, std_elapsed_png, mean_elapsed_dyn, std_elapsed_dyn, ...
-%     mean_f_png, std_f_png, mean_f_dyn, std_f_dyn)
+% h1 = boxplot(bp_elapsed_time_dyn, 'Color', 'r', ...
+%         'position', aux_position+0.1, 'Symbol', 'or', 'width', 0.3);
+% set(h1,{'linew'},{2}) 
 
+f_bp_time = figure('units','normalized','outerposition',[0 0 0.7 0.5]);
+aux_ = [bp_elapsed_time_png; bp_elapsed_time_dyn];
+group_idx = [ones(1, n_runs), 2*ones(1, n_runs)];
+group_names = {'PNG', 'Dynamics'};
+
+h = daboxplot(aux_,'groups', group_idx, 'outsymbol','ko',...
+    'fill',1,'legend',group_names(1:2), 'xtlabels', cond_name);
+grid on
+ylabel('Elapsed time [sec]')
+xtickangle(30)
+set(gca, 'Fontsize', 20)
+ylim([-5, 501])
+
+f_bp_fxe = figure('units','normalized','outerposition',[0 0 0.7 0.5]);
+aux_ = [bp_fxeq_png; bp_fxeq_dyn];
+group_idx = [ones(1, n_runs), 2*ones(1, n_runs)];
+group_names = {'PNG', 'Dynamics'};
+
+h = daboxplot(aux_,'groups', group_idx, 'outsymbol','ko',...
+    'fill',1,'legend',group_names(1:2), 'xtlabels', cond_name);
+grid on
+ylabel('f(x_e)')
+xtickangle(30)
+set(gca, 'Fontsize', 20, 'YScale', 'log')
+ylim([0, 1])
+
+ 
