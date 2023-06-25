3dc99ffedb1db8edb0b1e39076b1850f6cedf071
Sara <sommariva@dima.unige.it>
2023-06-25 15:52:56 +0200

    Added warning when no solution was found

--- funcs/f_NLPC_restart.m
+++ funcs/f_NLPC_restart.m
@@ -202,6 +202,10 @@ if (counter == max_counter) && (norm_F_x_new > tol)
     ir = ir+1; 
     FLAG = 0;
     clear upd_components cond_number zeri flag_save
+    if ir > num_try
+        warning(['The algorithm did not converge within the allowed number of iterations and restarts.' ...
+            'Results may be inaccurate and the problem may not have a solution.'])
+    end
 else
     % Step 6. Store results over run
     ris.x0 = x_0;
